ddb913efc64d615ca763d4e23d2ccfc0ec684353
Golen <golen87@hotmail.com>
2025-06-04 04:00:11 +0200

    Add card dragging

--- components/Card/Card.gd
+++ components/Card/Card.gd
@@ -4,13 +4,33 @@ extends Node2D
 @export var title: String = "Card Title"
 @export var description: String = "Effect text here"
 @export var cost: String = "1"
+@export var face_up: bool = true
 
+@onready var card_back: TextureRect = %CardBack
+@onready var card_front: TextureRect = %CardFront
 @onready var card_title: Label = %CardTitle
 @onready var card_description: RichTextLabel = %CardDescription
 @onready var card_cost: Label = %CardCost
 
 
 func _ready() -> void:
+	card_back.visible = not face_up
+	card_front.visible = face_up
 	card_title.text = title
 	card_description.text = description
 	card_cost.text = cost
+
+
+var is_held := false
+
+func _on_button_button_down() -> void:
+	is_held = true
+
+
+func _on_button_button_up() -> void:
+	is_held = false
+
+
+func _process(delta: float) -> void:
+	if is_held:
+		global_position = get_global_mouse_position()
