266ea7640db45b2a6744296c6d5ca95647220fc0
Naika <naika.artwork@gmail.com>
2025-08-17 14:30:30 +0200

    State machine demo

---
+++ game/scripts/state_machine/state.gd
@@ -0,0 +1,20 @@
+class_name State extends Node
+
+signal transitioned
+
+@onready var state_machine: StateMachine = get_parent()
+
+func enter():
+	pass
+
+func exit():
+	pass
+
+func update(delta):
+	pass
+
+func physics_update(delta):
+	pass
+
+func input(event):
+	pass
---
+++ game/scripts/state_machine/test_states/idle.gd
@@ -0,0 +1,9 @@
+class_name Idle extends State
+
+func enter():
+	#e.g. set sprite to idle and start timer
+	pass
+
+func update(delta):
+	#on tick logic
+	pass
