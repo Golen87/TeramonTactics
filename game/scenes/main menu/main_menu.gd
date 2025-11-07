9209df191a472248819567ac192289966dd455a6
Golen <golen87@hotmail.com>
2025-11-07 17:41:19 +0100

    Add animations scene to main menu.

--- game/scenes/main menu/main_menu.gd
+++ game/scenes/main menu/main_menu.gd
@@ -12,3 +12,6 @@ func _on_cards_pressed() -> void:
 
 func _on_attacks_pressed() -> void:
 	get_tree().change_scene_to_file(&"res://scenes/attack test/attack_test.tscn")
+
+func _on_animations_pressed() -> void:
+	get_tree().change_scene_to_file(&"res://scenes/animations/AnimationsScene.tscn")
